e57e126c5611e5b9d7c340a39eb694bda8ced044
Sam Scott <samuel_c_scott@hotmail.com>
2020-09-01 13:51:39 +0100

    Basic Project Scene Assembly
    Read this push title to the theme song of "Teenage Mutant Ninja Turtles"

--- ARTestProject/Assets/Scripts/ARTapToPlaceObj.cs
+++ ARTestProject/Assets/Scripts/ARTapToPlaceObj.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+//using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARSubsystems;
 
 public class ARTapToPlaceObj : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject placementIndicator;
+
+    [SerializeField]
+    //private ARSessionOrigin arOrigin;
+    private ARRaycastManager raycastManager;
+    private Pose placeObj;
+    private bool validPlacement = false;
     void Start()
     {
-        
+        raycastManager = FindObjectOfType<ARRaycastManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        UpdatePosePlacement();
+        UpdatePlaceIndicator();
+    }
+
+    private void UpdatePlaceIndicator()
+    {
+        if (validPlacement)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placeObj.position, placeObj.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePosePlacement()
+    {
+        var screenCentre = Camera.current.ViewportToScreenPoint(new Vector3(.5f,.5f));
+
+        var hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenCentre, hits, TrackableType.Planes);
+
+        validPlacement = hits.Count > 0;
+        if (validPlacement)
+        {
+            placeObj = hits[0].pose;
+        }
     }
 }
